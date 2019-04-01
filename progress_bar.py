17b663006a343665f960c776ecce2a9dbc9917fd
dantespeak - <herman.olmhagen@comgrail.se>
2019-04-01 18:51:20 +0200

    Made it even shittier

--- progress_bar.py
+++ progress_bar.py
@@ -1,6 +1,4 @@
 def progress_bar(count, _list):
-    rounded_perc = round((count/len(_list))*100)
-    print("|" + "#" * rounded_perc + "-" * (100-rounded_perc) + "|" + str(rounded_perc) + "%", end="\r", flush=True)
-
+    print("|" + "#" * round((count/len(_list))*100) + "-" * (100-round((count/len(_list))*100)) + "|" + str(round((count/len(_list))*100)) + "%", end="\r", flush=True)
     if len(_list)-1 == count:
         print("\n", flush=True)
